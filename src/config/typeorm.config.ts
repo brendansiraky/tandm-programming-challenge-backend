62408c5c1faeca27f94d8e599e1171f2ed06d046
brendansiraky <brendansirakydeveloper@gmail.com>
2022-09-27 08:17:44 +0800

    updadet typeorm config

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -2,7 +2,7 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
     type: 'postgres',
-    host: 'localhost',
+    host: 'db',
     port: 5432,
     username: 'postgres',
     password: 'postgres',
